671fee05b3bb8da2716278a75f9897e34b28f541
Jason Kinzer <j.kinzer.333@gmail.com>
2019-05-10 21:43:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson!";
 
-var greetSpecialGuest = "Neil deGrasse Tyson!" + specialGuest + "Hello, everybody!";
+var greetSpecialGuest = "Hello" + specialGuest + "Hello, everybody!";
 
 var topic = "Java"
 
-var conversation = `${topic}`;
+var conversation = `${java}`;
